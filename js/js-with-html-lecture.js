df3b7cb956cbc7fe66f3ca00194b3c0bc98a633c
Christian-A-Ramos <91964779+Christian-A-Ramos@users.noreply.github.com>
2021-10-08 11:25:59 -0500

    lecture examples

---
+++ js/js-with-html-lecture.js
@@ -0,0 +1,20 @@
+"use strict"
+
+var jeff = "jeff";
+console.log(jeff);
+
+console.log("Hello! Im in my own file now !")
+
+var returnOfAlert = alert("TGIF! Thank Goodness Im Fred!");
+console.log(returnOfAlert);
+
+var returnOfConfirm = confirm("Got any plans for this three day weekend ?");
+console.log(returnOfConfirm);
+
+var favoriteMetalBand = prompt(message: "whats your favorite new metal band?");
+console.log("ha ha, you like" + favoriteNuMetalBand + "!");
+
+var leastFavoriteSoda = prompt(message:"WHats your least favorite soda?");
+var favoriteSoda = prompt(message: "Whats your least favorite soda?")
+
+alert("Hello")
