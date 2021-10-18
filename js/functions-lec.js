a4a91f0c81b1292c6e3ab8e80af919b6c4778c5b
Christian-A-Ramos <91964779+Christian-A-Ramos@users.noreply.github.com>
2021-10-18 08:38:06 -0500

    all files up to date

---
+++ js/functions-lec.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// A function is a block of reusable code that performs a simple task.
+// Functions typically in an input, and produce an output.
+
+//Functions we have used so far:
+// - prompt();
+// - alert();
+// - Number();
+// - parseFloat();
+// - parseInt();
+// - .toFixed()
+// - .toUpperCase();
+
