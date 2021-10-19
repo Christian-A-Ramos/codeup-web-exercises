a571e44158c0082e24ce2ee12652bc1374fcb1af
Christian-A-Ramos <91964779+Christian-A-Ramos@users.noreply.github.com>
2021-10-19 14:58:01 -0500

    completed arrays exercise

---
+++ js/Morning_Warmups.js
@@ -0,0 +1,28 @@
+(function() {
+
+   // function returnSeven() {
+   //     return 7;
+   // }
+   //  console.log(returnSeven());
+   //
+   // if(false) {
+   //     console.log("Am i seen?");
+   // } else {
+   //     console.log("What about me?");
+   // }
+   // var hadBreakfast = Boolean(Math.round(Math.random()));
+   //
+   // var hadBreakfastMessage = (hadBreakfast) ? "That breakfast is sure good." : "I am veyr full.";
+   //
+   //  console.log(hadBreakfast);
+   //  console.log(hadBreakfastMessage);
+
+    for(var i = 1; i <= 100; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0)
+    }
+
+})()
---
+++ js/for_loops.js
@@ -0,0 +1,16 @@
+"use strict"
+
+(function() {
+
+function showMultiplicaionTable(num) {
+    for (var j = 1; j <= 10; j++) {
+        console.log(num + "x" + j + " = " + (num * j));
+    }
+}
+
+showMultiplicaionTable(7);
+
+
+
+
+})();
